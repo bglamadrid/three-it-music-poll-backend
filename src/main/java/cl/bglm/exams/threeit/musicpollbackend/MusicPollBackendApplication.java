80b292a814e4b6dad69acd6fc2cb3bf8ae95d352
bglamadrid <bg.lamadrid@gmail.com>
2022-09-29 20:56:18 -0300

    feat: initial commit
    Project structure generated using Spring Initializr

---
+++ src/main/java/cl/bglm/exams/threeit/musicpollbackend/MusicPollBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.bglm.exams.threeit.musicpollbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPollBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPollBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/bglm/exams/threeit/musicpollbackend/MusicPollBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bglm.exams.threeit.musicpollbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPollBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
