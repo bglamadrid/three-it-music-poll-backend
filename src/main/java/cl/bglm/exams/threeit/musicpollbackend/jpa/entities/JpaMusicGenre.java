b40420235d6e43bbc65de52fc68480a87240d404
bglamadrid <bg.lamadrid@gmail.com>
2022-09-30 13:39:57 -0300

    style: reorganize entity classes annotations
    lombok annotations are last
    `@Getter` and `@Setter` can be put at class level

--- src/main/java/cl/bglm/exams/threeit/musicpollbackend/jpa/entities/JpaMusicGenre.java
+++ src/main/java/cl/bglm/exams/threeit/musicpollbackend/jpa/entities/JpaMusicGenre.java
@@ -11,18 +11,18 @@ import javax.validation.constraints.NotBlank;
 /**
  * Entity class for music genres.
  */
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "music_genres")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class JpaMusicGenre {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "music_genre_id")
-  @Getter @Setter
   private Long id;
   @NotBlank
   @Column(name = "music_genre_name", length = 40, unique = true, nullable = false)
-  @Getter @Setter
   private String name;
 }
--- src/main/java/cl/bglm/exams/threeit/musicpollbackend/jpa/entities/JpaPollAnswer.java
+++ src/main/java/cl/bglm/exams/threeit/musicpollbackend/jpa/entities/JpaPollAnswer.java
@@ -10,23 +10,22 @@ import javax.persistence.*;
 /**
  * Entity class for poll answers.
  */
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "poll_answers")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class JpaPollAnswer {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "poll_answer_id", nullable = false)
-  @Getter @Setter
   private Long id;
 
   @Column(name = "poll_answer_mail", length = 100, nullable = false)
-  @Getter @Setter
   private String mail;
 
   @ManyToOne(cascade = CascadeType.REFRESH, optional = false)
   @JoinColumn(name = "music_genre_id", nullable = false)
-  @Getter @Setter
   private JpaMusicGenre music_genre;
 }
