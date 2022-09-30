ba8b7d2817cc276ac9667b102cc15c5699695fbd
bglamadrid <bg.lamadrid@gmail.com>
2022-09-30 12:59:40 -0300

    refactor: follow JPA buddy tip and remove `@Data` from entity

--- src/main/java/cl/bglm/exams/threeit/musicpollbackend/jpa/entities/JpaMusicGenre.java
+++ src/main/java/cl/bglm/exams/threeit/musicpollbackend/jpa/entities/JpaMusicGenre.java
@@ -1,25 +1,24 @@
 package cl.bglm.exams.threeit.musicpollbackend.jpa.entities;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 /**
  * Entity class for music genres.
  */
-@Data
 @Entity
 @Table(name = "music_genres")
 public class JpaMusicGenre {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "music_genre_id")
+  @Getter @Setter
   private Long id;
   @NotBlank
   @Column(name = "music_genre_name", length = 40, unique = true, nullable = false)
+  @Getter @Setter
   private String name;
 }
